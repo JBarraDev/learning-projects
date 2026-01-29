461b7113a8839639874a77f0d409fe8153e4d6e4
Javier Barra <revistaspgo@gmail.com>
2026-01-29 12:07:03 +0100

    feat: add Starship mapper (entity <-> responseDTO) and add dependency to mapper

---
+++ basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/mapper/StarshipMapper.java
@@ -0,0 +1,17 @@
+package com.projects.learning.mapper;
+
+import com.projects.learning.domain.Starship;
+import com.projects.learning.dto.StarshipResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface StarshipMapper {
+
+    StarshipResponseDTO toResponseDto(Starship entity);
+
+    @Mapping(target = "id", ignore = true)  // Se ignora id que genera solo
+    Starship toEntity(StarshipResponseDTO dto);
+}
+
+
