88c0a978f6d5065b34a3a8503cbfb9acb0f0cf9c
Javier Barra <revistaspgo@gmail.com>
2026-01-29 12:43:41 +0100

    fix: add getter and setter in Starship class

--- basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/domain/Starship.java
+++ basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/domain/Starship.java
@@ -1,10 +1,14 @@
 package com.projects.learning.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
 public class Starship {
 
     @Id
