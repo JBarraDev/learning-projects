a507d89e26080a1c2efec7c191af719231d185bd
Javier Barra <revistaspgo@gmail.com>
2026-01-29 11:41:05 +0100

    chore: add initial configuration files

---
+++ basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/StarNavRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.projects.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarNavRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarNavRegistryApplication.class, args);
+	}
+
+}
