a36d4e4b9e63dcc273b27470f9064e6553501ccd
Javier Barra <revistaspgo@gmail.com>
2026-01-31 13:26:29 +0100

    feat: add id attribute in Starship Response DTO

--- basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/dto/StarshipResponseDTO.java
+++ basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/dto/StarshipResponseDTO.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class StarshipResponseDTO {
 
+    private Long id;
     private String name;
     private String model;
     private Integer crewCapacity;
