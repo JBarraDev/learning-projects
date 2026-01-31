baef397349716d3536e285bd1294e7ae3f674e8a
Javier Barra <revistaspgo@gmail.com>
2026-01-31 13:27:38 +0100

    feat: change attribute List type and add @Getter anotation

--- basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/dto/StarshipStatsResponseDTO.java
+++ basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/dto/StarshipStatsResponseDTO.java
@@ -2,16 +2,18 @@ package com.projects.learning.dto;
 
 import com.projects.learning.domain.Starship;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
+@Getter
 @Setter
 @AllArgsConstructor
 public class StarshipStatsResponseDTO {
 
     private Integer numberOfShips;
     private Integer percentOfOperativeShips;
-    private List<Starship> starships;
+    private List<StarshipResponseDTO> starshipsWithMaxCrewCapacity;
 
 }
