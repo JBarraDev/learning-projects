7c8e21c549c9e525baf83437a6747bfa2b3ad874
Javier Barra <revistaspgo@gmail.com>
2026-01-31 12:52:15 +0100

    feat: add ShipStatus exception if value doesn't exist in the enum

--- basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/domain/ShipStatus.java
+++ basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/domain/ShipStatus.java
@@ -1,7 +1,22 @@
 package com.projects.learning.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Arrays;
+
 public enum ShipStatus {
     OPERATIONAL,
     UNDER_REPAIR,
-    DECOMMISSIONED
+    DECOMMISSIONED;
+
+    @JsonCreator
+    public static ShipStatus from(String value) {
+        return Arrays.stream(values())
+                .filter(v -> v.name().equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "El estado debe ser uno de: " + Arrays.toString(values())
+                ));
+    }
+
 }
