9f3c678c4506c3b569d926a586d7e9511cae2d4b
Javier Barra <revistaspgo@gmail.com>
2026-01-31 13:30:32 +0100

    feat: add Starship controller with 5 endpoints

---
+++ basic/Java-Spring-boot/StarNavRegistry/src/main/java/com/projects/learning/controller/StarshipController.java
@@ -0,0 +1,49 @@
+package com.projects.learning.controller;
+
+import com.projects.learning.dto.StarshipRequestDTO;
+import com.projects.learning.dto.StarshipResponseDTO;
+import com.projects.learning.dto.StarshipStatsResponseDTO;
+import com.projects.learning.dto.StatusUpdateDTO;
+import com.projects.learning.service.StarshipService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ships")
+public class StarshipController {
+
+    private StarshipService starshipService;
+
+    public StarshipController(StarshipService starshipService) {
+        this.starshipService = starshipService;
+    }
+
+    @GetMapping
+    ResponseEntity<List<StarshipResponseDTO>> getAllStarship(){
+        return ResponseEntity.ok(starshipService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<StarshipResponseDTO> getStarshipById(@PathVariable Long id){
+        return ResponseEntity.ok(starshipService.findById(id));
+    }
+
+    @PostMapping
+    ResponseEntity<StarshipResponseDTO> createStarship(@Valid @RequestBody StarshipRequestDTO starshipRequestDTO){
+        return ResponseEntity.ok(starshipService.save(starshipRequestDTO));
+    }
+
+    @PutMapping("{id}/status")
+    ResponseEntity<StarshipResponseDTO> updateStarshipStatus(@Valid @PathVariable Long id, @RequestBody StatusUpdateDTO status){
+        return ResponseEntity.ok(starshipService.updateStatus(id, status));
+    }
+
+    @GetMapping("/stats")
+    ResponseEntity<StarshipStatsResponseDTO> getStarshipStats(){
+        return ResponseEntity.ok(starshipService.getStats());
+    }
+
+}
